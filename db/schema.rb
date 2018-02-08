89f6d482865f10a884c19836a8d17a2b573e0498
Oscar Plaza <oscarplaza@ACAA064E.ipt.aol.com>
2018-02-08 11:37:09 -0300

    Recapitulacion para subir al nuevo servidor aws

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207192613) do
+ActiveRecord::Schema.define(version: 20180207193036) do
 
   create_table "empresas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "nombre"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20180207192613) do
     t.integer "usuario"
     t.integer "puerta"
     t.date "momento"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "permisos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "horarios"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
