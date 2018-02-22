f3d357e6271360381ecf05413d03bafdcd710e37
Oscar Plaza <oscarplaza@MacBook-de-Oscar.local>
2018-02-22 10:46:29 -0300

    change time zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   CIPPER = Gibberish::AES.new('Blast Door la lleva')
   def comprobacionMaestra(usuario,puerta)
-    day = Time.now.strftime("%A")
+    day = Time.zone.now.strftime("%A")
     horaActual = Time.zone.now
     id_day = getidday(day)
     permisosParaHoy =Permiso.where(usuario_id: usuario.id,puerta_id:puerta.id,dia_id:id_day).select(:horario_id).to_a
